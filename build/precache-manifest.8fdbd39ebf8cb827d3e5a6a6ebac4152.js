e02a3f911787a907f73590b3b5fbcd749bcd8033
dianesabra <dwsabra@gmail.com>
2019-04-23 21:10:09 -0500

    1

---
+++ build/precache-manifest.8fdbd39ebf8cb827d3e5a6a6ebac4152.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b518d53e00318990351c",
+    "url": "/clickgame2/static/js/runtime~main.70dd6d19.js"
+  },
+  {
+    "revision": "28f2d76f5043bbb37de9",
+    "url": "/clickgame2/static/js/main.bf80fda5.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickgame2/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "28f2d76f5043bbb37de9",
+    "url": "/clickgame2/static/css/main.c64d99e9.chunk.css"
+  },
+  {
+    "revision": "ad9b467b9db2aaa83161dff5664b2518",
+    "url": "/clickgame2/index.html"
+  }
+];
